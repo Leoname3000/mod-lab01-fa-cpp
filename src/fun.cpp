3cedce70b473d200cd57bf62d5ab9a4f1dbe9edd
Leonid Sulin <sulin.leonid@gmail.com>
2023-03-12 01:17:08 +0300

    Complete tasks

--- src/fun.cpp
+++ src/fun.cpp
@@ -1,14 +1,88 @@
 // Copyright 2022 UNN-IASR
+#include <cstring>
+#include <cctype>
+#include <cmath>
 #include "fun.h"
 
 unsigned int faStr1(const char *str) {
-    return 0;
+    unsigned int count = 0;
+    bool inWord = false;
+    bool hasDigit = false;
+    for (int i = 0; i < strlen(str); i++) {
+        if (str[i] != ' ' && !inWord) {
+            inWord = true;
+        }
+        if (inWord && isdigit(str[i])) {
+            hasDigit = true;
+        }
+        if (str[i] == ' ' && inWord) {
+            inWord = false;
+            if (!hasDigit) {
+                count++;
+            }
+            hasDigit = false;
+        }
+    }
+    if (inWord && !hasDigit) {
+        count++;
+    }
+    return count;
 }
 
 unsigned int faStr2(const char *str) {
-    return 0;
+    unsigned int count = 0;
+    bool inWord = false;
+    bool hasIllegalSymbols = false;
+    for (int i = 0; i < strlen(str); i++) {
+        if (str[i] != ' ') {
+            if (!inWord) {
+                inWord = true;
+                if (!isupper(str[i])) {
+                    hasIllegalSymbols = true;
+                }
+            }
+            else if (!islower(str[i])) {
+                hasIllegalSymbols = true;
+            }
+        }
+        if (str[i] == ' ' && inWord) {
+            inWord = false;
+            if (!hasIllegalSymbols) {
+                count++;
+            }
+            hasIllegalSymbols = false;
+        }
+    }
+    if (inWord && !hasIllegalSymbols) {
+        count++;
+    }
+    return count;
 }
 
 unsigned int faStr3(const char *str) {
-    return 0;
+    unsigned int symbols = 0;
+    unsigned int words = 0;
+    bool inWord = false;
+    for (int i = 0; i < strlen(str); i++) {
+        if (str[i] != ' ' && !inWord) {
+            inWord = true;
+        }
+        if (inWord) {
+            if (str[i] != ' ') {
+                symbols++;
+            }
+            else {
+                inWord = false;
+                words++;
+            }
+        }
+    }
+    if (inWord) {
+        words++;
+    }
+    if (words == 0) {
+        return 0;
+    }
+    double average = (double) symbols / words;
+    return (unsigned int) round(average);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
 // Copyright 2022 UNN-IASR
+#include <iostream>
 #include "fun.h"
 
 int main() {
+    char *str = (char*) "  Ab9cd   Abcd abc  AB";
+    std::cout << faStr1(str) << std::endl;
+    std::cout << faStr2(str) << std::endl;
+    std::cout << faStr3(str) << std::endl;
     return 0;
 }
